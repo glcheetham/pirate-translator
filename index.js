43b3aee02dfbf8fe5d77980e1fd2b2edef24712b
Gary Lewis Cheetham <yourstruly@glcheetham.name>
2016-06-24 16:39:13 +0100

    Made the translator ignore punctuation

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 const translator = require('./translator') // Translates individual words
 const postprocessor = require('./postprocessor') // Post-processor for individual words
 
-const input = "Hi I'm just testing my new pirate translator"
+const input = "Hi! I'm just testing my new pirate translator"
 
-const words = input.split(/\s/)
+const words = input.split(/\s/) // Split input around spaces
 
 words.forEach(word => {
-  let translation = word
-  translation = translator(word) || translation
+  let translation = word.replace(/[\!\?\'\"]/, "") // Remove common punctuation from the string
+  
+  translation = translator(translation) || translation
   translation = postprocessor(translation)
   console.log(translation)
 })
--- translator/index.js
+++ translator/index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 
 // return require() of the module in this directory, with the same name as "word", if it exists
-// This should be the "translation" module which contains the translation for "word"
+// This require() should be the "translation" module which contains the translation for "word"
 
 module.exports = function(word) {
   const translationPath = path.resolve(__dirname, './' + word.toLowerCase() + '.js')
