c68f05f68026f64d8b844ba15a44676518702052
Gary Lewis Cheetham <yourstruly@glcheetham.name>
2016-06-24 16:49:13 +0100

    Made the pirate translator export a function

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 const translator = require('./translator') // Translates individual words
 const postprocessor = require('./postprocessor') // Post-processor for individual words
 
-const input = "Hi! I'm just testing my new pirate translator"
+module.exports = function(input) {
+  const words = input.split(/\s/) // Split input around spaces
+  let translated = ""
 
-const words = input.split(/\s/) // Split input around spaces
+  words.forEach(word => {
+    let translation = word.replace(/[\!\?\'\"]/, "") // Remove common punctuation from the string
 
-words.forEach(word => {
-  let translation = word.replace(/[\!\?\'\"]/, "") // Remove common punctuation from the string
-  
-  translation = translator(translation) || translation
-  translation = postprocessor(translation)
-  console.log(translation)
-})
+    translation = translator(translation) || translation
+    translation = postprocessor(translation)
+
+    translated += translation + " "
+  })
+
+  // DODGY DODGY DODGY DODGY DODGY
+  translated = translated.slice(0, -1) // Remove the last characted (hopefully space) from the end of the string
+  return translated
+}
